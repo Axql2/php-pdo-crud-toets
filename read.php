ee45be5fb7ca77a8d1f5470394d69e030427ae43
Thomas Braakman 336469 <336469@student.mboutrecht.nl>
2023-01-10 12:21:31 +0100

    update 7

--- read.php
+++ read.php
@@ -21,10 +21,48 @@ $sql = "SELECT Id
 ,Model
 ,Topsnelheid
 ,Prijs
-FROM Persoon";
+FROM dureauto";
 
-echo $sql;
+$statement = $pdo->prepare($sql);
 
+$statement->execute();
+
+$result = $statement->fetchAll(PDO::FETCH_OBJ);
+
+$tableRows = "";
+
+foreach($result as $info) {
+    $tableRows .= "<tr>
+                        <td>$info->Id</td>
+                        <td>$info->Merk</td>
+                        <td>$info->Model</td>
+                        <td>$info->Topsnelheid</td>
+                        <td>$info->Prijs</td>
+                        <td>
+                            <a href='delete.php?Id=$info->Id'>
+                            <img src='b_drop.png' alt='cross'>
+                            </a>
+                         </td>   
+                   </tr>";
+}
 
 
 ?>
+  <h3>De vijf duurste auto's ter wereld</h3>
+    <a href="index.php">
+        <input type="button" value="Maak een nieuw record">
+    </a>
+
+<br><br>
+<table border='1'>
+    <thead>
+        <th>Voornaam</th>
+        <th>Tussenvoegsel</th>
+        <th>Achternaam</th>`
+        <th>Delete</th>
+        <th>Edit</th>
+    </thead>
+    <tbody>
+        <?php echo $tableRows; ?>
+    </tbody>
+</table>
